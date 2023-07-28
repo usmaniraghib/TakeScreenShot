d87e797d3693e708f4c821dafdf15fcd30605d91
usmaniraghib <javaprogrammers786@gmail.com>
2023-07-28 12:42:51 +0530

    Warning Removed

--- src/cm/raghib/selenium/TakesScreenshotPartial.java
+++ src/cm/raghib/selenium/TakesScreenshotPartial.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
