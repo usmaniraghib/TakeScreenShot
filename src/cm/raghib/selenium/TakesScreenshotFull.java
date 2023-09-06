8b8d6f65c6e6a799b4fafd79027e53ff6082625b
usmaniraghib <javaprogrammers786@gmail.com>
2023-09-06 16:45:05 +0530

    Converted into maven project.

--- src/cm/raghib/selenium/TakesScreenshotFull.java
+++ src/cm/raghib/selenium/TakesScreenshotFull.java
@@ -2,26 +2,31 @@ package cm.raghib.selenium;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TakesScreenshotFull {
+public class TakesScreenshotFull extends BaseClass {
+
+	public static WebDriver driver;
+	public static String browserName = "chrome";
+	public static String browserVersion = "116";
+	
+	public static String url = "http://google.com";
+	
 	public static void main(String[] args) throws IOException {
-		// Set the driver path
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\Driver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		// Chrome Browser
+		driver = BaseClass.getDriver(browserName, browserVersion);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.manage().window().maximize();
-		driver.get("http://google.com");
+		driver.get(url);
 		
 		//CASTING DRIVER OBJECT TO TAKE SCREEN SHOTS.
 		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 		FileUtils.copyFile(src, new File(System.getProperty("user.dir")+"\\screenshot.png"));
-		driver.quit();
-
+		BaseClass.quitDriver();
 	}
-
 }
--- src/cm/raghib/selenium/TakesScreenshotPartial.java
+++ src/cm/raghib/selenium/TakesScreenshotPartial.java
@@ -2,38 +2,42 @@ package cm.raghib.selenium;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TakesScreenshotPartial {
+public class TakesScreenshotPartial extends BaseClass {
 
+	public static WebDriver driver;
+	public static String browserName = "chrome";
+	public static String browserVersion = "116";
+	
 	public static String location1 = "http://google.com";
 	public static String location2 = "https://rahulshettyacademy.com/angularpractice/";
 
 	public static String serachBoxXPath = "//body/div[1]/div[3]/form[1]/div[1]/div[1]/div[1]/div[1]/div[2]/input[1]";
 	public static String nameBoxXPath = "//body/app-root[1]/form-comp[1]/div[1]/form[1]/div[1]/input[1]";
 
+	
 	public static void main(String[] args) throws IOException, InterruptedException {
-		// Set the driver path
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\Driver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		// Chrome Browser
+		driver = BaseClass.getDriver(browserName, browserVersion);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.manage().window().maximize();
-
-		/*
-		 * driver.get(location1); WebElement searchBox =
-		 * driver.findElement(By.xpath(serachBoxXPath));
-		 * searchBox.sendKeys("What is selenium webdriver");
-		 * 
-		 * // CASTING DRIVER OBJECT TO TAKE SCREEN SHOTS. File src1 =
-		 * searchBox.getScreenshotAs(OutputType.FILE); FileUtils.copyFile(src1, new
-		 * File(System.getProperty("user.dir") + "\\searchBoxPartialScreenshot.png"));
-		 * Thread.sleep(5000); driver.close();
-		 */
+		
+//		driver.get(location1);
+//		WebElement searchBox = driver.findElement(By.xpath(serachBoxXPath));
+//		searchBox.sendKeys("What is selenium webdriver");
+//
+//		// CASTING DRIVER OBJECT TO TAKE SCREEN SHOTS. 
+//		File src1 =	searchBox.getScreenshotAs(OutputType.FILE);
+//		FileUtils.copyFile(src1, new File(System.getProperty("user.dir") + "\\searchBoxPartialScreenshot.png"));
+//		Thread.sleep(5000);
+//		BaseClass.closeDriver(); 
 
 		driver.get(location2);
 		WebElement nameBox = driver.findElement(By.xpath(nameBoxXPath));
@@ -42,8 +46,6 @@ public class TakesScreenshotPartial {
 		File src2 = nameBox.getScreenshotAs(OutputType.FILE);
 		FileUtils.copyFile(src2, new File(System.getProperty("user.dir") + "\\nameBoxPartialScreenshot.png"));
 		Thread.sleep(5000);
-		driver.quit();
-
+		BaseClass.quitDriver();
 	}
-
 }
